6f875c790bea32f254346d838cfb1e4a29663e00
Thomas Bonfort <thomas.bonfort@airbus.com>
2024-02-14 09:28:57 +0100

    switch to universal container

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-#echo HOME=$HOME
-#echo USER=$USER
-## install gdal gs:// handler
-#git clone -q https://github.com/airbusgeo/godal
-#cd godal/gdalplugin
-#make
-#sudo cp gdal_gcs.so /usr/lib/x86_64-linux-gnu/gdalplugins/
-#cd ../..
-#rm -rf godal
-echo export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/gcloud/application_default_credentials.json >> $HOME/.bashrc
-echo export GODAL_NUMBLOCKS=1000 >> $HOME/.bashrc
-echo export GODAL_BLOCKSIZE=128k >> $HOME/.bashrc
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install -y google-cloud-cli
+
+pip3 install -r requirements.txt
 
 
