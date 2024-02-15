34f38e2777784d8ec7b7dc3724b18b3eed214ef0
Thomas Bonfort <thomas.bonfort@airbus.com>
2024-02-15 14:33:32 +0100

    update devcontainer env

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install -y google-cloud-cli
-
-pip3 install -r requirements.txt
-
-
